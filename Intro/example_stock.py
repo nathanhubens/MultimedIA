fc02d91c67d226b9e7240787ac07eab8b91b1ec3
Maio175 <antoine.maiorca@umons.ac.be>
2021-10-18 17:18:15 +0200

    add pptx and misc codes

---
+++ Intro/example_stock.py
@@ -0,0 +1,22 @@
+import altair as alt
+import ipywidgets as widgets
+from vega_datasets import data
+
+source = data.stocks()
+
+stock_picker = widgets.SelectMultiple(
+    options=source.symbol.unique(),
+    value=list(source.symbol.unique()),
+    description='Symbols')
+
+# The value of symbols will come from the stock_picker.
+@widgets.interact(symbols=stock_picker)
+def render(symbols):
+  selected = source[source.symbol.isin(list(symbols))]
+
+  return alt.Chart(selected).mark_line().encode(
+      x='date',
+      y='price',
+      color='symbol',
+      strokeDash='symbol',
+  )
