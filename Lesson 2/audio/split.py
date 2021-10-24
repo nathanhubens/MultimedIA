b24672ce902cb03d4092b99292026e23124a2bd3
Maio175 <antoine.maiorca@umons.ac.be>
2021-10-24 17:15:00 +0200

    audio classification seance 2

---
+++ Lesson 2/audio/split.py
@@ -0,0 +1,9 @@
+# Helper function to split the data
+def CrossValidationSplitter(col='fold', fold=1):
+    "Split `items` (supposed to be a dataframe) by fold in `col`"
+    def _inner(o):
+        assert isinstance(o, pd.DataFrame), "ColSplitter only works when your items are a pandas DataFrame"
+        col_values = o.iloc[:,col] if isinstance(col, int) else o[col]
+        valid_idx = (col_values == fold).values.astype('bool')
+        return IndexSplitter(mask2idxs(valid_idx))(o)
+    return _inner
